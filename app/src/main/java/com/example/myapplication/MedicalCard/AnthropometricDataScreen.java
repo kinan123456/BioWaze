2e51b17bff7dc48e602ac5313bacac38ba1d58b4
kinan123456 <kinansobih29@gmail.com>
2019-05-30 04:15:54 +0100

    1

--- app/src/main/java/com/example/myapplication/MedicalCard/AnthropometricDataScreen.java
+++ app/src/main/java/com/example/myapplication/MedicalCard/AnthropometricDataScreen.java
@@ -4,17 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.R;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AnthropometricDataScreen extends AppCompatActivity {
+
+    private EditText weight, height, bloodPressure, WaistCircumference, pulse;
 
     public void SubmitClick(View view){
 
     }
 
+    public void initVariables(){
+        weight = (EditText) findViewById(R.id.Weight);
+        height = (EditText) findViewById(R.id.Height);
+        WaistCircumference = (EditText) findViewById(R.id.WaistCircumference);
+        pulse = (EditText) findViewById(R.id.Pulse);
+        bloodPressure = (EditText) findViewById(R.id.BloodPressure);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anthropometric_data_screen);
+        setTitle("Anthropometric Data");
+        initVariables();
     }
 }
